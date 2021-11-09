2ab59945502cf4c25095b2c152366dcc18f86033
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-11-09 11:36:29 +0300

    feat: input stream

---
+++ app/inputStream.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+exports.inputStream = (filename) => {
+  if (filename) {
+    if (fs.existsSync(filename)) {
+      return fs.createReadStream(filename);
+    } else {
+      console.error("Input file does not exist");
+      process.exit(9);
+    }
+  }
+
+  return process.stdin;
+};
+
