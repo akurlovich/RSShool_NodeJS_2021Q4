1ba02995e88d52a806480e5c563186a218d059e8
akurlovich <71250705+akurlovich@users.noreply.github.com>
2021-11-14 22:55:50 +0300

    feat: custom read and write streams

--- app/inputStream.js
+++ app/inputStream.js
@@ -1,15 +1,12 @@
 const fs = require('fs');
+const myRead = require('./customRead.js')
 
 exports.inputStream = (filename) => {
   if (filename) {
-    if (fs.existsSync(filename)) {
-      return fs.createReadStream(filename);
-    } else {
-      console.error("Input file does not exist");
-      process.exit(9);
-    }
+    const newMyRead = new myRead(filename)
+    return newMyRead;
+  } else {
+    return process.stdin;
+    
   }
-
-  return process.stdin;
 };
-
--- app/outputStream.js
+++ app/outputStream.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
+const myWrite = require('./customWrite.js');
 
 exports.outputStream = (filename) => {
   if (filename) {
-    if (fs.existsSync(filename)) {
-      return fs.createWriteStream(filename, {flags: 'a'});
+    const newMyWrite = new myWrite(filename);
+    return newMyWrite;
     } else {
-      console.error("Output file does not exist");
-      process.exit(9);
+      return process.stdout;
+      
     }
-  }
 
-  return process.stdout;
 }
