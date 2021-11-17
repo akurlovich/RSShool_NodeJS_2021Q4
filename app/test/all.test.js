9fbc4bff6a97f916fd32b871f367b702fec93a66
akurlovich <71250705+akurlovich@users.noreply.github.com>
2021-11-17 20:34:34 +0300

    fix

--- app/test/all.test.js
+++ app/test/all.test.js
@@ -25,7 +25,7 @@ describe('Test CLI config', () => {
   });
 
   test('Valid input file flag', () => {
-    expect(getInput('-i')).toBe('-1234');
+    expect(getInput('-i')).toBe('-klj');
   });
 
   test('Valid input doble file flag', () => {
@@ -37,7 +37,7 @@ describe('Test CLI config', () => {
   });
 
   test('Valid ouput file flag', () => {
-    expect(getOutput('-1234')).toBe('-lll');
+    expect(getOutput('-klj')).toBe('-lll');
   });
 
   test('Valid ouput file flag', () => {
